0ceb21e1413c6ccab80a4dbeecd1fd8e89669918
uxiomerino <78788071+uxiomerino@users.noreply.github.com>
2021-04-20 19:07:18 +0200

    Iteracion4SinMapa

--- Servicios Web/Servicios Web.py
+++ Servicios Web/Servicios Web.py
@@ -7,7 +7,7 @@ Created on Tue Apr  6 09:22:07 2021
 import requests
 from bs4 import BeautifulSoup
 import gmg
-#from gmg import plotMap
+from gmg import plotMap
 
 
 
@@ -35,74 +35,85 @@ for elemento in parser.find_all("tr"):
                 
 puntos=[]
 t=0
-
-
-for x in range(len(listacódigos)):
-    x=0
-    identificador = listacódigos[t]
-    concello = listaconcellos[t]
-
-print(identificador, concello)
+while t<15: #Para mostrar más de 15 ayuntamientos basta con cambiar este bucle while aumentando el 15 por el número deseado
     
-vtiempo=requests.get("http://servizos.meteogalicia.gal/rss/predicion/jsonPredConcellos.action?idConc="+identificador )
-tiempo= vtiempo.json()
-predicion_tiempo= tiempo['predConcello']['listaPredDiaConcello'][0]['ceo']['manha']
-
-
-codpred=requests.get("https://irdgcdinfo.data.blog/codigos/")
-codigo=codpred.content
-codigoparse=BeautifulSoup(codigo, "html.parser")
-lista3=[]
-for elemento in codigoparse.find_all("tr"):
+    for x in range(len(listacódigos)):
+        x=0
+        identificador = listacódigos[t]
+        concello = listaconcellos[t]
     
-    for elemento in codigoparse.find_all("th"):
+    print(identificador, concello)
         
-        elemento=elemento.text
-        if elemento.isdecimal()==True:
-            pass
-        elif elemento == "Valor numérico" or elemento== "Descrición do estado do ceo" or elemento=="Non dispoñible":
-            pass
-        else:
-            lista3.append(elemento)
+    vtiempo=requests.get("http://servizos.meteogalicia.gal/rss/predicion/jsonPredConcellos.action?idConc="+identificador )
+    tiempo= vtiempo.json()
+    predicion_tiempo= tiempo['predConcello']['listaPredDiaConcello'][0]['ceo']['manha']
+    
+
+    codpred=requests.get("https://irdgcdinfo.data.blog/codigos/")
+    codigo=codpred.content
+    codigoparse=BeautifulSoup(codigo, "html.parser")
+    lista3=[]
+    for elemento in codigoparse.find_all("tr"):
+        
+        for elemento in codigoparse.find_all("th"):
             
-for i in range(len(lista3)):
-    i=predicion_tiempo-100
-    predicionceo=lista3[i]
+            elemento=elemento.text
+            if elemento.isdecimal()==True:
+                pass
+            elif elemento == "Valor numérico" or elemento== "Descrición do estado do ceo" or elemento=="Non dispoñible":
+                pass
+            else:
+                lista3.append(elemento)
+                
+    for i in range(len(lista3)):
+        i=predicion_tiempo-100
+        predicionceo=lista3[i]
+        
+    print(predicionceo)
+
+
+    coordenadasconcello= {
+            'key': 'b8fb3471f4b042',
+            'q': concello ,
+            'format': 'xml'
+                }
     
-print(predicionceo)
-
-
-coordenadasconcello= {
-        'key': 'b8fb3471f4b042',
-        'q': concello ,
-        'format': 'xml'
-            }
-
-bcoordenadas = requests.get("https://us1.locationiq.com/v1/search.php", params = coordenadasconcello)
+    bcoordenadas = requests.get("https://us1.locationiq.com/v1/search.php", params = coordenadasconcello)
    
-bcoordenadas1= bcoordenadas.text
-
-parseo= BeautifulSoup(bcoordenadas1, 'lxml')
-
-listacoords=[]
-listalat=[]
-listalon=[]
-   
-for elemento in parseo.find_all("place"):
-    important= elemento['importance']
-    lat= elemento['lat']
-    lon=elemento['lon']
-    listacoords.append(important)
-    listalat.append(lat)
-    listalon.append(lon)
- 
-for i in range(len(listacoords)):
-    coordenadas=listacoords[0]
+    bcoordenadas1= bcoordenadas.text
     
-for i in range(len(listalat)):
-    latitud=listalat[0]
-
-for i in range(len(listalon)):
-    longitud=listalon[0]
-
-print('Coordenadas', latitud,  longitud) 
+    parseo= BeautifulSoup(bcoordenadas1, 'lxml')
+    
+    listacoords=[]
+    listalat=[]
+    listalon=[]
+   
+    for elemento in parseo.find_all("place"):
+        important= elemento['importance']
+        lat= elemento['lat']
+        lon=elemento['lon']
+        listacoords.append(important)
+        listalat.append(lat)
+        listalon.append(lon)
+     
+    for i in range(len(listacoords)):
+        coordenadas=listacoords[0]
+        
+    for i in range(len(listalat)):
+        latitud=listalat[0]
+    
+    for i in range(len(listalon)):
+        longitud=listalon[0]
+    
+    print('Coordenadas', latitud,  longitud) 
+    
+    coords1=float(longitud)
+    coords2=float(latitud)
+    coordstotales=(coords1,coords2)
+    
+    final=(predicion_tiempo, coordstotales)
+    puntos.append(final)
+    
+    t+=1
+    
+plotMap(points = puntos)
